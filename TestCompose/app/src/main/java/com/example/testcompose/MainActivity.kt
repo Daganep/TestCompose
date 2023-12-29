e7c9d977b9b4d6ad58fd176f0c9bce3edc752ec2
Daganep <Daganep@yandex.ru>
2023-12-29 17:12:02 +0300

    init

---
+++ TestCompose/app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.testcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.testcompose.ui.theme.TestComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { }
+    }
+}
